e7d1fe2ed5a1462492c2f12407601f5e32ad7676
ashna23 <ashnaraikwar9@gmail.com>
2026-02-01 13:24:39 +0530

    recursion

--- function.c
+++ function.c
@@ -1,17 +1,36 @@
-#include<stdio.h>
-void printHello(); //declaration
-void printGoodbye();
-int main(){
-    printGoodbye();
-    printHello(); //functionc call
-    return 0;
-}
+// #include<stdio.h>
+// void printHello(); //declaration
+// void printGoodbye();
+// int main(){
+//     printGoodbye();
+//     printHello(); //functionc call
+//     return 0;
+// }
 
-void printHello(){
-    printf("hello");
+// void printHello(){
+//     printf("hello");
 
-}
-void printGoodbye(){
-    printf("goodbye \n");
+// }
+// void printGoodbye(){
+//     printf("goodbye \n");
     
+// }
+
+
+
+#include<stdio.h>
+//REcurrsion
+void printHL(int count);
+
+int main(){
+  printHL(6);
+  return 0;
+}
+
+void printHL(int count){
+ if(count == 0){
+    return;
+ }
+printf("helloWorld \n");
+printHL(count-1);
 }
