6f425c1604feb75d52894a9c65c91688a0b41daf
ashna23 <ashnaraikwar9@gmail.com>
2026-02-01 15:26:40 +0530

    sumofnatural

---
+++ questions/ch5fun/sumNaturalNo.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ 
+int  sum(int n);
+
+int main(){
+  int n;
+    printf("Enter a natural number: ");
+    scanf("%d", &n);
+
+    printf("Sum of first %d natural numbers = %d", n, sum(n));
+    return 0;
+    
+}
+
+
+int sum(int n){
+    if (n==1){
+        return 1;}
+        else{
+        return n+sum (n-1);
+}}
