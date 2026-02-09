123e37bcf9e12da3ed39e425e11378ca25b0f22a
ashna23 <ashnaraikwar9@gmail.com>
2026-02-09 12:39:05 +0530

    read

--- ch1file.c
+++ ch1file.c
@@ -4,6 +4,11 @@ int main(){
     FILE *fptr;
      fptr= fopen("test.txt","r");
 //                filename,Mode
+char ch;
+for(int i=1;i<=5;i++){
+    fscanf(fptr,"%c",&ch);
+printf("character = %c\n",ch);
+}
 fclose(fptr);
     return 0;
 }
